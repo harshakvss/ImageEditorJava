f1401f789594f3a96db1f5213f2dacaf06c7d5fa
nikhilbisht <nikhilbisht96@gmail.com>
2017-02-16 14:17:39 +0530

    Add files via upload

---
+++ Editor.java
@@ -0,0 +1,135 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.image.*;
+import java.awt.color.*;
+import javax.swing.filechooser.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.geom.*;
+import javax.imageio.*;
+import javax.imageio.stream.*;
+
+////start the ImgArea class
+//The ImgArea class acts as a drawing area of the Image Editor program
+
+class ImgArea extends Canvas{ 
+
+  Image Img;
+  BufferedImage BufferedImg;
+  BufferedImage bufimg; 
+  BufferedImage bufimg1; 
+  float e;
+  float radian;
+  Dimension ds;
+  int mX;
+  int mY;
+  int x;
+  int y;
+  static boolean imgLoad;
+  boolean actionSlided;
+  boolean actionResized;
+  boolean actionCompressed;
+  boolean actionTransparent;
+  boolean actionRotated;
+  boolean actionDraw;
+  boolean drawn;
+  MediaTracker mt;
+  static Color c;
+  Color colorTextDraw;
+  Robot rb;
+  boolean dirHor;
+  String imgFileName;
+  String fontName;
+  int fontSize;
+  String textToDraw;
+  public ImgArea(){
+
+   
+  
+	  addMouseListener(new mousexy());
+	 
+	   addKeyListener(new KeyList()); 
+
+	   try{
+	    rb=new Robot(); 
+	   }catch(AWTException e){}
+
+	   ds=getToolkit().getScreenSize();    
+	   mX=(int)ds.getWidth()/2; 
+	   mY=(int)ds.getHeight()/2;
+	   
+	  }
+	  
+	  public void paint(Graphics g){
+	   Graphics2D g2d=(Graphics2D)g;   
+	   if(imgLoad){
+
+		    
+		    if(actionSlided || actionResized || actionTransparent || actionRotated || drawn ){
+		     x=mX-bufimg.getWidth()/2;
+		     y=mY-bufimg.getHeight()/2;
+		     g2d.translate(x,y);  
+		     g2d.drawImage(bufimg,0,0,null); 
+		     
+		     }
+	 
+		    else{
+	     
+	   
+	     x=mX-BufferedImg.getWidth()/2;
+	     y=mY-BufferedImg.getHeight()/2;
+	     g2d.translate(x,y); 
+	     g2d.drawImage(BufferedImg,0,0,null); 
+	     
+		    }}
+	   g2d.dispose(); 
+	   
+	  }
+
+	  class mousexy extends MouseAdapter{
+	   
+	   public void mousePressed(MouseEvent e){
+	 
+	   }
+	   
+	   
+	  }
+	  
+
+	
+
+	 
+	  class KeyList extends KeyAdapter{
+	   public void keyPressed(KeyEvent e){
+	    if(e.getKeyCode()==27){ 
+	     actionDraw=false;
+	     textToDraw="";
+	     fontName="";
+	     fontSize=0;
+	     }
+	    }
+	   }
+
+  
+  public void prepareImg(String filename){
+   
+   try{
+  
+   mt=new MediaTracker(this);    
+   Img=Toolkit.getDefaultToolkit().getImage(filename); 
+   mt.addImage(Img,0);
+    mt.waitForID(0); 
+    
+   int width=Img.getWidth(null);
+   int height=Img.getHeight(null);
+   
+   BufferedImg=createBufferedImageFromImage(Img,width,height,false);
+   
+   bufimg = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);  
+   imgLoad=true;
+   }catch(Exception e){System.exit(-1);}
+  }
+
+  
